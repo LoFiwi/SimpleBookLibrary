3427e1e77610154461b188bc5e92e4c0baf54a93
mr.lofiwi <romaatasovmusic@gmail.com>
2025-06-12 00:26:48 +0300

    Add simple functional(add books, save list and load list).

--- SimpleBookLibrary/Form1.Designer.cs
+++ SimpleBookLibrary/Form1.Designer.cs
@@ -28,13 +28,147 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.textBoxAuthor = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.dataGridViewBooks = new System.Windows.Forms.DataGridView();
+            this.buttonAdd = new System.Windows.Forms.Button();
+            this.buttonSave = new System.Windows.Forms.Button();
+            this.buttonLoad = new System.Windows.Forms.Button();
+            this.textBoxTitle = new System.Windows.Forms.Label();
+            this.textBoxYear = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewBooks)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBoxAuthor
+            // 
+            this.textBoxAuthor.AutoSize = true;
+            this.textBoxAuthor.Location = new System.Drawing.Point(12, 50);
+            this.textBoxAuthor.Name = "textBoxAuthor";
+            this.textBoxAuthor.Size = new System.Drawing.Size(37, 13);
+            this.textBoxAuthor.TabIndex = 0;
+            this.textBoxAuthor.Text = "Автор";
+            this.textBoxAuthor.Click += new System.EventHandler(this.bookAuthor_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(89, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // dataGridViewBooks
+            // 
+            this.dataGridViewBooks.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewBooks.Location = new System.Drawing.Point(12, 198);
+            this.dataGridViewBooks.Name = "dataGridViewBooks";
+            this.dataGridViewBooks.Size = new System.Drawing.Size(776, 240);
+            this.dataGridViewBooks.TabIndex = 2;
+            this.dataGridViewBooks.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridViewBooks_CellContentClick);
+            // 
+            // buttonAdd
+            // 
+            this.buttonAdd.Location = new System.Drawing.Point(12, 148);
+            this.buttonAdd.Name = "buttonAdd";
+            this.buttonAdd.Size = new System.Drawing.Size(90, 35);
+            this.buttonAdd.TabIndex = 3;
+            this.buttonAdd.Text = "Додати";
+            this.buttonAdd.UseVisualStyleBackColor = true;
+            this.buttonAdd.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // buttonSave
+            // 
+            this.buttonSave.Location = new System.Drawing.Point(349, 148);
+            this.buttonSave.Name = "buttonSave";
+            this.buttonSave.Size = new System.Drawing.Size(87, 35);
+            this.buttonSave.TabIndex = 3;
+            this.buttonSave.Text = "Зберегти";
+            this.buttonSave.UseVisualStyleBackColor = true;
+            this.buttonSave.Click += new System.EventHandler(this.buttonSave_Click);
+            // 
+            // buttonLoad
+            // 
+            this.buttonLoad.Location = new System.Drawing.Point(703, 148);
+            this.buttonLoad.Name = "buttonLoad";
+            this.buttonLoad.Size = new System.Drawing.Size(85, 35);
+            this.buttonLoad.TabIndex = 3;
+            this.buttonLoad.Text = "Завантажити";
+            this.buttonLoad.UseVisualStyleBackColor = true;
+            this.buttonLoad.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBoxTitle
+            // 
+            this.textBoxTitle.AutoSize = true;
+            this.textBoxTitle.Location = new System.Drawing.Point(12, 12);
+            this.textBoxTitle.Name = "textBoxTitle";
+            this.textBoxTitle.Size = new System.Drawing.Size(71, 13);
+            this.textBoxTitle.TabIndex = 0;
+            this.textBoxTitle.Text = "Назва книги";
+            this.textBoxTitle.Click += new System.EventHandler(this.textBoxTitle_Click);
+            // 
+            // textBoxYear
+            // 
+            this.textBoxYear.AutoSize = true;
+            this.textBoxYear.Location = new System.Drawing.Point(12, 90);
+            this.textBoxYear.Name = "textBoxYear";
+            this.textBoxYear.Size = new System.Drawing.Size(65, 13);
+            this.textBoxYear.TabIndex = 0;
+            this.textBoxYear.Text = "Рік випуску";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(89, 50);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 1;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(89, 90);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 1;
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.buttonLoad);
+            this.Controls.Add(this.buttonSave);
+            this.Controls.Add(this.buttonAdd);
+            this.Controls.Add(this.dataGridViewBooks);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.textBoxYear);
+            this.Controls.Add(this.textBoxTitle);
+            this.Controls.Add(this.textBoxAuthor);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewBooks)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label textBoxAuthor;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.DataGridView dataGridViewBooks;
+        private System.Windows.Forms.Button buttonAdd;
+        private System.Windows.Forms.Button buttonSave;
+        private System.Windows.Forms.Button buttonLoad;
+        private System.Windows.Forms.Label textBoxTitle;
+        private System.Windows.Forms.Label textBoxYear;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
     }
 }
 
--- SimpleBookLibrary/Form1.cs
+++ SimpleBookLibrary/Form1.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using System.Text.Json;
+using System.IO;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -8,6 +10,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+
 namespace SimpleBookLibrary
 {
     public partial class Form1 : Form
@@ -16,5 +19,92 @@ namespace SimpleBookLibrary
         {
             InitializeComponent();
         }
+
+        private List<Book> books = new List<Book>();
+        private readonly string path = Path.Combine(Application.StartupPath, "books.json"); // Path to the JSON file with book data.
+
+
+
+        private void button1_Click(object sender, EventArgs e)  // Add Book Button.
+        {
+            string title = textBox1.Text;
+            string author = textBox2.Text;
+            if (!int.TryParse(textBox3.Text, out int year))
+            {
+                MessageBox.Show("Невірний рік!");
+                return;
+            }
+
+            Book book = new Book { Title = title, Author = author, Year = year };
+            books.Add(book);
+            UpdateGrid();
+        }
+
+        private void UpdateGrid()
+        {
+            dataGridViewBooks.DataSource = null;
+            dataGridViewBooks.DataSource = books;
+        }
+
+        private void button3_Click(object sender, EventArgs e)  // Load Books Button.
+        {
+            if (File.Exists(path))
+            {
+                string json = File.ReadAllText("books.json");
+                books = JsonSerializer.Deserialize<List<Book>>(json) ?? new List<Book>();
+                UpdateGrid();
+                MessageBox.Show("Завантажено!");
+            }
+        }
+
+        private void bookAuthor_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonSave_Click(object sender, EventArgs e)   // Save Books Button.
+        {
+            string json = JsonSerializer.Serialize(books);
+            File.WriteAllText(path, json);
+            MessageBox.Show("Збережено!");
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dataGridViewBooks_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBoxTitle_Click(object sender, EventArgs e)
+        {
+
+        }
     }
+    [Serializable]
+    public class Book
+    {
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public int Year { get; set; }
+    }
+
 }
